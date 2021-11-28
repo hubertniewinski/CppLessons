d7f136099f860cc5b45f9fa7e2c7fccf1d32b995
Hubert Niewiński <hubertniewinski@yahoo.com>
2021-11-28 17:08:07 +0100

    assignment_04_init

---
+++ Assignment_04/cmake-build-debug/CMakeFiles/Assignment_04.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Assignment_04.exe"
+  "Assignment_04.exe.manifest"
+  "Assignment_04.lib"
+  "Assignment_04.pdb"
+  "CMakeFiles/Assignment_04.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment_04.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
